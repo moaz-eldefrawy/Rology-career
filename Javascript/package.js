7e5cdd1a52085f520cfb802cdd5cf9cbfc4afe05
Moaz El-defrawy <moaz.eldefrawy@gmail.com>
2018-10-05 21:47:54 +0200

    first msg

---
+++ Javascript/package.js
@@ -0,0 +1,16 @@
+
+Package.describe({
+  name    : 'semantic:ui-menu',
+  summary : 'Semantic UI - Menu: Single component release',
+  version : '2.2.10',
+  git     : 'git://github.com/Semantic-Org/UI-Menu.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    'menu.css',
+    'grid.css',
+    'divider.css'
+  ], 'client');
+});
